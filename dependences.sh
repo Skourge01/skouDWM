e2078af9928a49e726238059d03507b605ba6e89
Skourge <oditorrinco222@gmail.com>
2024-12-29 17:55:10 -0300

    Update dependences.sh

--- dependences.sh
+++ dependences.sh
@@ -3,7 +3,7 @@
 # Script para instalar pacotes básicos no Arch Linux
 # Inclui: fonte Fira Code, gerenciador de arquivos Thunar, Firefox, feh e Font Awesome
 
-sudo pacman --noconfirm -S firefox ttf-fira-code ttf-font-awesome thunar xorg-server xorg-xinit libx11 libxft libxinerama libxrandr lxappearance materia-gtk-theme feh dmenu  || {
+sudo pacman --noconfirm -S firefox ttf-fira-code ttf-font-awesome thunar xorg-server xorg-xinit libx11 libxft libxinerama libxrandr lxappearance materia-gtk-theme feh dmenu easyeffects || {
     echo "Erro na instalação dos pacotes"
     exit 1
 }
