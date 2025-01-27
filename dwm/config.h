19c7f36bbbd9d39a5643f28dd4439ea397439448
Skourge01 <oditorrinco222@gmail.com>
2025-01-27 15:19:31 -0300

    mudance in rules, odder aplications

--- dwm/config.h
+++ dwm/config.h
@@ -27,8 +27,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Discord",   "discord",  NULL,  0,   1,  -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Alacritty", NULL,       NULL,       0,            0,           -1 }
+	{ "VSCodium",  NULL,       NULL,       1 << 1,       0,           0 },
 };
 
 /* layout(s) */
